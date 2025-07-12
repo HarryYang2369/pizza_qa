48a6a25d5c519ba5455efc1bf776fdb9df33de2b
HarryYang2369 <y.yang-343@sms.ed.ac.uk>
2025-07-12 18:28:57 +0800

    finished the construction of teacher panel

--- qa/admin.py
+++ qa/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import YearGroup, Question, Answer
 
-# Register your models here.
+@admin.register(YearGroup)
+class YearGroupAdmin(admin.ModelAdmin):
+    list_display = ('year',)
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'student', 'year_group', 'created_at', 'resolved')
+    search_fields = ('title', 'description')
+    list_filter = ('year_group', 'resolved')
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('text', 'user', 'question', 'created_at')
+    search_fields = ('text',)
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,14 @@
+"""Define URL patterns for the users app"""
 from django.urls import path
 from . import views
 
+app_name = 'users'  # Add this line to register the namespace
+
 urlpatterns = [
     path('register/student/', views.register_student, name='register_student'),
-    path('register/teacher/', views.register_teacher, name='register_teacher'),
+    #path('register/teacher/', views.register_teacher, name='register_teacher'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('management/', views.student_management, name='student_management'),
+    path('delete/<int:student_id>/', views.delete_student, name='delete_student'),
 ]
